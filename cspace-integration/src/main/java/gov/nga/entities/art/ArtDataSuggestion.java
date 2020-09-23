a615578fcf0c1cb27b8d072c24318a2450f9fb6c
Charles Alers <c-alers@nga.gov>
2020-09-23 13:29:45 -0400

    IRISSEARCH-21: Duplicate Suggestions
    The hashCode for ArtDataSuggestion has been updated to reflect the logic of the equals method.

--- cspace-integration/src/main/java/gov/nga/entities/art/ArtDataSuggestion.java
+++ cspace-integration/src/main/java/gov/nga/entities/art/ArtDataSuggestion.java
@@ -1,11 +1,16 @@
 package gov.nga.entities.art;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import gov.nga.common.suggest.Suggestion;
 import gov.nga.common.utils.StringUtils;
 import gov.nga.common.utils.TypeUtils;
 
 public class ArtDataSuggestion extends Suggestion
 {
+	private static final Logger log = LoggerFactory.getLogger(ArtDataSuggestion.class);
+	
 	Long entityID = null;
     
     public ArtDataSuggestion(final String compareString, final String displayString, final Long entityID) 
@@ -22,7 +27,7 @@ public class ArtDataSuggestion extends Suggestion
     @Override
     public String toString()
     {
-        return StringUtils.remove(StringUtils.removeHTML(getDisplayString()),'"');
+        return String.format("ArtDataSuggestion: %s %d", StringUtils.remove(StringUtils.removeHTML(getDisplayString()),'"'), getEntityID());
     }
 
     @Override
@@ -34,6 +39,7 @@ public class ArtDataSuggestion extends Suggestion
             ArtDataSuggestion other = (ArtDataSuggestion) o;
             isEqual = other.entityID.equals(entityID) && getDisplayString().toLowerCase().equals(other.getDisplayString().toLowerCase());
         }
+        if (entityID == 17805L) log.info(String.format("%s <> %s : %b", this, o, isEqual));
         return isEqual;
     }
 
@@ -44,6 +50,6 @@ public class ArtDataSuggestion extends Suggestion
             return 0;
         if (getDisplayString() == null)
             return Long.valueOf(entityID).hashCode();
-        return (Long.valueOf(entityID).hashCode() >> 13) ^ getDisplayString().hashCode();
+        return (Long.valueOf(entityID).hashCode() >> 13) ^ getDisplayString().toLowerCase().hashCode();
     }
 }
